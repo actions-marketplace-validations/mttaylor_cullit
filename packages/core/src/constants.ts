1075b5de4ccf8c9ae5b56276fdcb13f9c0b99073
mttaylor <matttaylor.ce@gmail.com>
2026-04-06 13:39:26 -0600

    chore: bump version to v2.3.1

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,6 +1,6 @@
 // Shared constants across Cullit packages
 
-export const VERSION = '2.3.0';
+export const VERSION = '2.3.1';
 
 export const DEFAULT_CATEGORIES = ['features', 'fixes', 'breaking', 'improvements', 'chores'];
 
